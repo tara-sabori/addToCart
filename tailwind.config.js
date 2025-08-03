463a12ebe417154381bc44313d1a5480d252883c
tara-sabori <tarasaboori@gmail.com>
2025-08-03 20:06:51 +0330

    setup tailwind css and initial project configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,34 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        900: `rgb(var(--color-primary-900))`,
+        800: `rgb(var(--color-primary-800))`,
+        700: `rgb(var(--color-primary-700))`,
+        600: `rgb(var(--color-primary-600))`,
+        500: `rgb(var(--color-primary-500))`,
+        400: `rgb(var(--color-primary-400))`,
+        300: `rgb(var(--color-primary-300))`,
+        200: `rgb(var(--color-primary-200))`,
+        100: `rgb(var(--color-primary-100))`,
+      },
+      secondary: {
+        900: `rgb(var(--color-secondary-900))`,
+        800: `rgb(var(--color-secondary-800))`,
+        700: `rgb(var(--color-secondary-700))`,
+        600: `rgb(var(--color-secondary-600))`,
+        500: `rgb(var(--color-secondary-500))`,
+        400: `rgb(var(--color-secondary-400))`,
+        300: `rgb(var(--color-secondary-300))`,
+        200: `rgb(var(--color-secondary-200))`,
+        100: `rgb(var(--color-secondary-100))`,
+      },
+      orange:{
+        950:'rgb(249, 181, 0)'
+      },
+      backgroundapp:`rgb(var(--background-app-rgb))` 
+    }
   },
   plugins: [],
 }
